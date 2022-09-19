7ddbadc9a8b8bd755991c5bbdbff0a66aef90a53
Victor Kane <victorkane@gmail.com>
2022-09-19 10:32:59 -0300

    build(color-mode): add dependencies and configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   content: {
     documentDriven: true
   }
